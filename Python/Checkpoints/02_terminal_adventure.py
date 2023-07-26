fcdea966d8b1021a35c8fdda41f73c1d2873b030
Nea <nealeonhard@gmail.com>
2023-07-26 22:34:36 +0200

    añadido definiciiones de dinos y stats a cada jugador

--- Python/Checkpoints/02_terminal_adventure.py
+++ Python/Checkpoints/02_terminal_adventure.py
@@ -21,7 +21,7 @@ print("|  __ \   / _ \  | |/ /  |  ____| |  \/  |  / _ \  | \ | |")
 print("| |__) | | | | | |   /   | |___   | \  / | | | | | |  \| |")
 print("|  ___/  | | | | |  <    |  ___|  | |\/| | | | | | | . ` |")
 print("| |      | |_| | | . \   | |____  | |  | | | |_| | | |\  |")
-print("|_|       \___/  |_|\_\  |______| |_|  |_|  \___/  |_| \_| By Nearius")
+print("|_|       \___/  |_|\_\  |______| |_|  |_|  \___/  |_| \_|")
 print("")
 
 print(" _________________________________________________________________________ ")
@@ -31,47 +31,129 @@ print(f"|{BWHITE}{BLACK}  trainer starts by choosing their very first Pokémon. 
 print(f"|{BWHITE}{BLACK}  to make that choice. Will it be the fiery Charmander, the water-loving  {RESET}|")
 print(f"|{BWHITE}{BLACK}  Squirtle, or the plant-powered Bulbasaur? The decision is yours!        {RESET}|")
 print(f"|{BWHITE}__________________________________________________________________________{RESET}|")
+print(" ")
 
-# I will store each Pokémon with its variable indicating the color it will have, so the code will be cleaner from now on."
+
+# I will store each Pokémon with its variable indicating the color it will have, so the code will be cleaner from now on.and also his attacks, hp, and lvl's"
 
 Ch = (f"{RED}Charmander{RESET}")
+Ch_LvL = 5 ; Ch_HP = 28
+Ch_attack1_name= "Ember" ; Ch_attack1_damage = 6
+Ch_attack2_name= "Scratch" ; Ch_attack2_damage = 5
+Ch_attack3_name= "Flamethrower" ; Ch_attack3_damage = 8
+Ch_attack4_name= "Dragon Breath" ; Ch_attack4_damage = 7
+
 Sq = (f"{BLUE}Squirtle{RESET}")
+Sq_LvL = 5 ; Sq_HP = 30
+Sq_attack1_name= "Water Gun" ; Sq_attack1_damage = 6
+Sq_attack2_name= "Tackle" ; Sq_attack2_damage = 5
+Sq_attack3_name= "Bubble" ; Sq_attack3_damage = 6
+Sq_attack4_name= "Hydro Pump" ; Sq_attack4_damage = 8
+
 Bu = (f"{GREEN}Bulbasaur{RESET}")
+Bu_LvL = 5 ; Bu_HP = 29
+Bu_attack1_name= "Vine Whip" ; Bu_attack1_damage = 6
+Bu_attack2_name= "Headbutt" ; Bu_attack2_damage = 5
+Bu_attack3_name= "Leech Seed" ; Bu_attack3_damage = 6
+Bu_attack4_name= "Razor Leaf" ; Bu_attack4_damage = 7
 
 print(" ________________________________________________________________________")
 print(f"|   🔥 {Ch} 🔥   |    💧 {Sq} 💧     |     🌱 {Bu} 🌱     |")   #I know this might bother perfectionists like me, but I have to leave it this way to print correctly in the log.
 print("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|")    
-print("| 1. Ember              | 1. Water Gun         | 1. Vine Whip            |")
-print("| 2. Scratch            | 2. Tackle            | 2. Growl                |")
-print("| 3. Flamethrower       | 3. Bubble            | 3. Leech Seed           |")
-print("| 4. Dragon Breath      | 4. Hydro Pump        | 4. Razor Leaf           |")
+print(f"| 1. {Ch_attack1_name}              | 1. {Sq_attack1_name}         | 1. {Bu_attack1_name}            |")
+print(f"| 2. {Ch_attack2_name}            | 2. {Sq_attack2_name}            | 2. {Bu_attack2_name}             |")
+print(f"| 3. {Ch_attack3_name}       | 3. {Sq_attack3_name}            | 3. {Bu_attack3_name}           |")
+print(f"| 4. {Ch_attack4_name}      | 4. {Sq_attack4_name}        | 4. {Bu_attack4_name}           |")
 print("|_______________________|______________________|_________________________|")
 print("")
+print("")
 
-# Now it's time for the player to choose their main Pokémon. I will use a while loop to ensure the player picks a number between 1, 2, or 3. Originally, the main character's rival in Pokémon was 'Gary,' the nephew of Professor Oak. We will also define the Pokémon that your adversary will choose in this game."
+# Now it's time for the player to choose their main Pokémon. First I create the variable for the pokemon and his atributes for each trainer, then I will use a "while" loop to ensure the player picks a number between 1, 2, or 3. Originally, the main character's rival in Pokémon was 'Gary,' the grandson of Professor Oak. We will also define the Pokémon that your adversary will choose in this game."
 
-mypokemon = 0
-garypokemon = 0
+MyPokemon = 0
+My_LvL = 0 ; My_HP = 0 ; My_xp = 0
+My_attack1_name= " " ; My_attack1_damage = 0
+My_attack2_name= " " ; My_attack2_damage = 0
+My_attack3_name= " " ; My_attack3_damage = 0
+My_attack4_name= " " ; My_attack4_damage = 0
 
-while mypokemon != 1 and  mypokemon != 2 and mypokemon != 3:
-    mypokemon = int(input(f"Time to choose your starter Pokémon! Type {BOLDWHITE}1{RESET} for {Ch}, {BOLDWHITE}2{RESET} for {Sq}, or {BOLDWHITE}3{RESET} for {Bu}: "))
-    if mypokemon != 1 and  mypokemon != 2 and mypokemon != 3:
-        print(f"⚠️  Error: You selected '{mypokemon}' but only options 1, 2, or 3 are valid.")
+GaryPokemon = 0
+Ga_LvL = 0 ; My_HP = 0 ; Ga_xp = 0
+Ga_attack1_name= " " ; Ga_attack1_damage = 0
+Ga_attack2_name= " " ; Ga_attack2_damage = 0
+Ga_attack3_name= " " ; Ga_attack3_damage = 0
+Ga_attack4_name= " " ; Ga_attack4_damage = 0
+
+while MyPokemon != 1 and  MyPokemon != 2 and MyPokemon != 3:
+    MyPokemon = int(input(f"Time to choose your starter Pokémon! Type {BOLDWHITE}1{RESET} for {Ch}, {BOLDWHITE}2{RESET} for {Sq}, or {BOLDWHITE}3{RESET} for {Bu}: "))
+    if MyPokemon != 1 and  MyPokemon != 2 and MyPokemon != 3:
+        print(f"⚠️  Error: You selected '{MyPokemon}' but only options 1, 2, or 3 are valid.")
         print(" ")
     else:
         print(" ")
 
-#Now Ill difine the pokemon picked for player and Gary
+#Now Ill difine the pokemon picked for player and Gary and his respective atributes
 
-if mypokemon == 1:
-    mypokemon = Ch
-    garypokemon = Sq
-elif mypokemon == 2:
-    mypokemon = Sq
-    garypokemon = Bu
+if MyPokemon == 1:
+    MyPokemon = Ch
+    My_LvL = Ch_LvL ; My_HP = Ch_HP ;  My_xp = 0
+    My_attack1_name= Ch_attack1_name ; My_attack1_damage = Ch_attack1_damage
+    My_attack2_name= Ch_attack2_name ; My_attack2_damage = Ch_attack2_damage
+    My_attack3_name= Ch_attack3_name ; My_attack3_damage = Ch_attack3_damage
+    My_attack4_name= Ch_attack4_name ; My_attack4_damage = Ch_attack4_damage
+
+    GaryPokemon = Sq
+    Ga_LvL = Sq_LvL ; Ga_HP = Sq_HP ;  Ga_xp = 0
+    Ga_attack1_name= Sq_attack1_name ; Ga_attack1_damage = Sq_attack1_damage
+    Ga_attack2_name= Sq_attack2_name ; Ga_attack2_damage = Sq_attack2_damage
+    Ga_attack3_name= Sq_attack3_name ; Ga_attack3_damage = Sq_attack3_damage
+    Ga_attack4_name= Sq_attack4_name ; Ga_attack4_damage = Sq_attack4_damage
+
+elif MyPokemon == 2:
+    MyPokemon = Sq
+    My_LvL = Sq_LvL ; My_HP = Sq_HP ;  My_xp = 0
+    My_attack1_name= Sq_attack1_name ; My_attack1_damage = Sq_attack1_damage
+    My_attack2_name= Sq_attack2_name ; My_attack2_damage = Sq_attack2_damage
+    My_attack3_name= Sq_attack3_name ; My_attack3_damage = Sq_attack3_damage
+    My_attack4_name= Sq_attack4_name ; My_attack4_damage = Sq_attack4_damage
+
+    GaryPokemon = Bu
+    Ga_LvL = Bu_LvL ; Ga_HP = Bu_HP ;  Ga_xp = 0
+    Ga_attack1_name= Bu_attack1_name ; Ga_attack1_damage = Bu_attack1_damage
+    Ga_attack2_name= Bu_attack2_name ; Ga_attack2_damage = Bu_attack2_damage
+    Ga_attack3_name= Bu_attack3_name ; Ga_attack3_damage = Bu_attack3_damage
+    Ga_attack4_name= Bu_attack4_name ; Ga_attack4_damage = Bu_attack4_damage
+
 else:
-    mypokemon = Bu
-    garypokemon = Ch  
+    MyPokemon = Bu
+    My_LvL = Bu_LvL ; My_HP = Bu_HP ;  My_xp = 0
+    My_attack1_name= Bu_attack1_name ; My_attack1_damage = Bu_attack1_damage
+    My_attack2_name= Bu_attack2_name ; My_attack2_damage = Bu_attack2_damage
+    My_attack3_name= Bu_attack3_name ; My_attack3_damage = Bu_attack3_damage
+    My_attack4_name= Bu_attack4_name ; My_attack4_damage = Bu_attack4_damage
+
+    GaryPokemon = Ch
+    Ga_LvL = Ch_LvL ; Ga_HP = Ch_HP ;  Ga_xp = 0
+    Ga_attack1_name= Ch_attack1_name ; Ga_attack1_damage = Ch_attack1_damage
+    Ga_attack2_name= Ch_attack2_name ; Ga_attack2_damage = Ch_attack2_damage
+    Ga_attack3_name= Ch_attack3_name ; Ga_attack3_damage = Ch_attack3_damage
+    Ga_attack4_name= Ch_attack4_name ; Ga_attack4_damage = Ch_attack4_damage
+
+
+print(MyPokemon)
+print(My_LvL, My_HP, My_xp) 
+print(My_attack1_name, My_attack1_damage) 
+print(My_attack2_name, My_attack2_damage)
+print(My_attack3_name, My_attack3_damage )
+print(My_attack4_name, My_attack4_damage) 
+
+print(" ")
+print(GaryPokemon)
+print(Ga_LvL, Ga_HP, Ga_xp) 
+print(Ga_attack1_name, Ga_attack1_damage) 
+print(Ga_attack2_name, Ga_attack2_damage)
+print(Ga_attack3_name, Ga_attack3_damage )
+print(Ga_attack4_name, Ga_attack4_damage) 
 
 
 print(" __________________________________________________________________ ")
@@ -85,7 +167,7 @@ print(f"|{BWHITE}_______________________________________________________________
 print(" ")
 print(" ")
 print("Gary: Ready to learn how real battle Pokemon is done? Let's see if your Pokémon can stand up to mine!")
-print (f"{ITALICS}Gary has sent out {garypokemon}!")
+print (f"{ITALICS}Gary has sent out {GaryPokemon}!")
 print(" ")
 print("You: Bring it on, Gary! I believe in my Pokémon, even if we've just started our journey together!")
-print(f"{ITALICS}You have sent our {mypokemon}!")
+print(f"{ITALICS}You have sent our {MyPokemon}!")
